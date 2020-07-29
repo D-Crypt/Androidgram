d5cff473dad5398729b8da53a8f8f8cc94da48b8
D-Crypt <danielvaughan54@gmail.com>
2020-07-29 14:38:08 +0100

    Define select photo method

--- app/src/main/java/com/example/androidgram/MainActivity.java
+++ app/src/main/java/com/example/androidgram/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.androidgram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view);
+    }
+
+    public void selectPhoto() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
     }
 }
